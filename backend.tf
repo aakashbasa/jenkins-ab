edabc42b8d20eca06d9c21c8e780d450ad11e8b4
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2022-05-26 17:48:39 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-    backend "s3" {
-      bucket = "aboi"
-      key    = "terraform/lambda/terraform_jenkins.tfstate"
-      region = "us-east-1"
-    }
-}
+# terraform {
+#     backend "s3" {
+#       bucket = "aboi"
+#       key    = "terraform/lambda/terraform_jenkins.tfstate"
+#       region = "us-east-1"
+#     }
+# }
