362de597147c3e5eb996eb471e50054a82334303
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2022-05-26 23:18:14 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,11 @@ provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
+
+terraform {
+ backend "s3" {
+   bucket          = "terraform-state-lr60nw3l"
+   key             = "terraform.tfstate"
+   region          = "us-east-1"
+ }
+}
