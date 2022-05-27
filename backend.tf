8c8e6b3bf1849d045a9af45bbf8c46e8ea6b21a6
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2022-05-26 23:39:20 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
-# terraform {
-#  backend "s3" {
-#    bucket          = "terraform-state-lr60nw3l"
-#    key             = "terraform.tfstate"
-#    region          = "us-east-1"
-#    access_key      = var.aws_access_key_id
-#    secret_key      = var.aws_secret_access_key
-#  }
-# }
+terraform {
+ backend "s3" {
+   bucket          = "terraform-state-lr60nw3l"
+   key             = "terraform.tfstate"
+   region          = "us-east-1"
+ }
+}
