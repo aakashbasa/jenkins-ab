061dadbceba80ef263a420c0810cfbfddb019957
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2022-05-26 21:50:02 -0400

    remove unwanted lines

--- key.tf
+++ key.tf
@@ -1,11 +1,3 @@
-# resource "aws_key_pair" "mykeypair" {
-#   key_name   = "mykeypair"
-#   public_key = fileexists(var.PATH_TO_PUBLIC_KEY) ? file(var.PATH_TO_PUBLIC_KEY) : var.DUMMY_SSH_PUB_KEY
-#   lifecycle {
-#     ignore_changes = [public_key]
-#   }
-# }
-
 data "aws_key_pair" "jenkins" {
   key_name = "mykeypair"
 }
