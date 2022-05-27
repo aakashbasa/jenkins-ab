7caac1ec39649e6bbf62b4ee40077ef9e7537135
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2022-05-26 21:45:55 -0400

    use existing key pair

--- key.tf
+++ key.tf
@@ -1,8 +1,11 @@
-resource "aws_key_pair" "mykeypair" {
-  key_name   = "mykeypair"
-  public_key = fileexists(var.PATH_TO_PUBLIC_KEY) ? file(var.PATH_TO_PUBLIC_KEY) : var.DUMMY_SSH_PUB_KEY
-  lifecycle {
-    ignore_changes = [public_key]
-  }
-}
+# resource "aws_key_pair" "mykeypair" {
+#   key_name   = "mykeypair"
+#   public_key = fileexists(var.PATH_TO_PUBLIC_KEY) ? file(var.PATH_TO_PUBLIC_KEY) : var.DUMMY_SSH_PUB_KEY
+#   lifecycle {
+#     ignore_changes = [public_key]
+#   }
+# }
 
+data "aws_key_pair" "jenkins" {
+  key_name = "mykeypair"
+}
